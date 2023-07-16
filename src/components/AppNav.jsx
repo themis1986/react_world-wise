0adff67e510a1226b5f36949906c689b68ba8839
themis1986 <themis_1986@hotmail.com>
2023-07-16 23:21:12 +0300

    Build app layout, add nested routes

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,5 +1,17 @@
 import styles from "./AppNav.module.css";
+import { NavLink } from "react-router-dom";
 
 export default function AppNav() {
-  return <nav className={styles.nav}>App navigation</nav>;
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+import AppNav from "./AppNav";
+import Logo from "./Logo";
+import styles from "./Sidebar.module.css";
+
+export default function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <Outlet />
+
+      <footer className={styles.footer}>
+        <p className={styles.copoyright}>
+          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+        </p>
+      </footer>
+    </div>
+  );
+}
