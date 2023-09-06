e32de80cad94b8baae4a11e0697f1c087ac997a0
themis1986 <themis_1986@hotmail.com>
2023-09-06 22:16:14 +0300

    Delete a city

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -51,7 +51,19 @@ function CitiesProvider({ children }) {
       const data = await res.json();
       setCities((cities) => [...cities, data]);
     } catch (error) {
-      alert("There was an error loading data...");
+      alert("There was an error creating the city...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`, {});
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch (error) {
+      alert("There was an error deleting city...");
     } finally {
       setIsLoading(false);
     }
@@ -59,7 +71,7 @@ function CitiesProvider({ children }) {
 
   return (
     <CitiesContext.Provider
-      value={{ cities, isLoading, currentCity, getCity, createCity }}
+      value={{ cities, isLoading, currentCity, getCity, createCity, deleteCity }}
     >
       {children}
     </CitiesContext.Provider>
